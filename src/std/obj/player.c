b66b5e5e89572611a8df5ce7d0ea3dc528dcecf8
Shea690901 <ginny690901@hotmail.de>
2016-01-11 16:53:24 +0100

    <feat>(player): added std player object
    - supports query_cwd needed by simul_efun

---
+++ src/std/obj/player.c
@@ -0,0 +1,19 @@
+/**
+* @file player.c
+* @brief
+* @author Gwenhyvar
+* @version 0.0.0
+* @date 2016-01-11
+*/
+
+private nosave string cwd;          ///< current working directory
+
+/**
+* @brief query_cwd
+*
+* @return curent working directory
+*/
+public string query_cwd()
+{
+    return cwd;
+}
