c80579b6a92d80621bdfd1aafc4cbe6195f92125
Shea690901 <ginny690901@hotmail.de>
2016-01-11 15:13:10 +0100

    <feat>(std_paths.h): added include file for standard pathes

---
+++ src/secure/include/std_paths.h
@@ -0,0 +1,35 @@
+/// @addtogroup privileged
+/// @{
+/// @file std_paths.h
+/// @brief standard defines for priviledged objects
+/// @author Gwenhwyvar
+/// @version 0.0.0
+/// @date 2015-11-29
+
+#ifndef __SEC_STD_PATHS_H
+#define __SEC_STD_PATHS_H
+
+// directories {{{
+#define LOG_DIR         "/var/log/"                 ///< logfiles
+#define PRIV_SAVE_DIR   "/var/save/"                ///< save-files of privileged objects
+#define CFG_DIR         "/var/cfg/"                 ///< base config files
+#define SECURE_DIR      "/secure/"                  ///< everything security relevant
+#define DAEMON_DIR      SECURE_DIR "daemons/"       ///< daemons
+#define SEFUN_DIR       SECURE_DIR "sefuns/"        ///< simul_efun-modules
+// }}}
+
+// files {{{
+#define PRELOADS        CFG_DIR "PRELOADS"          ///< file containing filenames to be preloaded
+// }}}
+
+// objects {{{
+#define LOGIN_OB        SECURE_DIR "login"          ///< login object
+#define SEFUNS          SECURE_DIR "simul_efun"     ///< simul efuns
+// }}}
+
+// daemons {{{
+// }}}
+
+#endif // __SEC_STD_PATHS_H
+
+///  @}
