a60751f521ad4210ef8d6ded8b2f0feda3f48623
Shea690901 <ginny690901@hotmail.de>
2016-02-02 22:52:52 +0100

    <feat>(menu): added menu system

---
+++ src/secure/include/menu.h
@@ -0,0 +1,7 @@
+#ifndef __SEC_MENU_H
+#define __SEC_MENU_H
+
+#define MENU		struct menu
+#define MENU_ITEM	struct menu_item
+
+#endif  // __SEC_MENU_H
--- src/secure/include/std_paths.h
+++ src/secure/include/std_paths.h
@@ -12,48 +12,51 @@
 #include <config.h>
 
 // directories
-#define LOG_DIR         "/var/log/"                     ///< logfiles
-#define PRIV_SAVE_DIR   "/var/save/"                    ///< save-files of privileged objects
-#define PLAYER_SAVE_DIR "/var/player_save/"             ///< save-files of players
-#define CFG_DIR         "/var/cfg/"                     ///< base config files
-#define SECURE_DIR      "/secure/"                      ///< everything security relevant
-#define DAEMON_DIR      SECURE_DIR "daemons/"           ///< daemons
-#define SECURE_OBJ_DIR  SECCURE_DIR "obj/"              ///< security relevant std objects
-#define SECURE_LIB_DIR  SECCURE_DIR "lib/"              ///< security relevant std library
-#define SEFUN_DIR       SECURE_DIR "sefuns/"            ///< simul_efun-modules
+#define LOG_DIR         "/var/log/"                         ///< logfiles
+#define PRIV_SAVE_DIR   "/var/save/"                        ///< save-files of privileged objects
+#define PLAYER_SAVE_DIR "/var/player_save/"                 ///< save-files of players
+#define CFG_DIR         "/var/cfg/"                         ///< base config files
+#define SECURE_DIR      "/secure/"                          ///< everything security relevant
+#define DAEMON_DIR      SECURE_DIR "daemons/"               ///< daemons
+#define SECURE_OBJ_DIR  SECCURE_DIR "obj/"                  ///< security relevant std objects
+#define SECURE_LIB_DIR  SECCURE_DIR "lib/"                  ///< security relevant std library
+#define SEFUN_DIR       SECURE_DIR "sefuns/"                ///< simul_efun-modules
 
-#define __HOME_DIR(x)   "/players/" x[0..0] "/" x "/"   ///< create home directory name
+#define __HOME_DIR(x)   "/players/" x[0..0] "/" x "/"       ///< create home directory name
 
 // files
-#define __SAVE_FILE(x)      ((x) + __SAVE_EXT)          ///< create save file name (uncompressed)
-#define __SAVE_GZ_FILE(x)   ((x) + __SAVE_GZ_EXT)       ///< create save file name (compressed)
+#define __SAVE_FILE(x)      ((x) + __SAVE_EXT)              ///< create save file name (uncompressed)
+#define __SAVE_GZ_FILE(x)   ((x) + __SAVE_GZ_EXT)           ///< create save file name (compressed)
 /// create save file name for given player
 #define __PLAYER_SAVE(x)    __SAVE_FILE(PLAYER_SAVE_DIR  x[0..0] "/" x)
 
-#define PRELOADS        CFG_DIR "PRELOADS"              ///< file containing filenames to be preloaded
+#define PRELOADS        CFG_DIR "PRELOADS"                  ///< file containing filenames to be preloaded
 
-#define ACL_READ_CFG    CFG_DIR "Read.acl"              ///< standard acl for read access
-#define ACL_WRITE_CFG   CFG_DIR "Write.acl"             ///< standard acl for write access
-#define PRIVS_CFG       CFG_DIR "Privs.cfg"             ///< default privs for all objects
+#define ACL_READ_CFG    CFG_DIR "Read.acl"                  ///< standard acl for read access
+#define ACL_WRITE_CFG   CFG_DIR "Write.acl"                 ///< standard acl for write access
+#define PRIVS_CFG       CFG_DIR "Privs.cfg"                 ///< default privs for all objects
 
-#define WELCOME_MSG     CFG_DIR "welcome.txt"           ///< welcome message, displayed @ logon
-#define MOTD            CFG_DIR "motd.txt"              ///< message of the day, displayed @ logon
+#define WELCOME_MSG     CFG_DIR "welcome.txt"               ///< welcome message, displayed @ logon
+#define MOTD            CFG_DIR "motd.txt"                  ///< message of the day, displayed @ logon
 
 // library
-#define P_INPUT         SECURE_LIB_DIR "player/inp_sys"     ///< player module for input handling
-#define P_MESSAGE       SECURE_LIB_DIR "player/messages"    ///< player module for message handling
-#define P_SHELL         SECURE_LIB_DIR "player/shell"       ///< player module for shell handling
+#define P_INPUT         SECURE_LIB_DIR "player/inp_sys"     ///< module for input handling
+#define P_MESSAGE       SECURE_LIB_DIR "player/messages"    ///< module for message handling
+#define P_SHELL         SECURE_LIB_DIR "player/shell"       ///< module for shell handling
+
+#define M_INPUT         SECURE_LIB_DIR "input"              ///< helper module for player input
+#define M_MENU          SECURE_LIB_DIR "menu"               ///< standard module for menus
 
 // objects
 #define LOGIN_OB        SECURE_OBJ_DIR "player/login"       ///< login object
 #define PLAYER_OB       SECURE_OBJ_DIR "player/player"      ///< player object
 
 // daemons
-#define MUD_INFO_D      DAEMON_DIR "mud_info"           ///< mud infos
-#define SYSLOG_D        DAEMON_DIR "syslogd"            ///< logging daemon
-#define TMP_D           DAEMON_DIR "tmpd"               ///< handler for temporary files
-#define MAIL_D          DAEMKN_DIR "smtpd"              ///< mailer daemon
-#define NEWS_D          DAEMKN_DIR "newsd"              ///< news daemon
+#define MUD_INFO_D      DAEMON_DIR "mud_info"               ///< mud infos
+#define SYSLOG_D        DAEMON_DIR "syslogd"                ///< logging daemon
+#define TMP_D           DAEMON_DIR "tmpd"                   ///< handler for temporary files
+#define MAIL_D          DAEMKN_DIR "smtpd"                  ///< mailer daemon
+#define NEWS_D          DAEMKN_DIR "newsd"                  ///< news daemon
 
 #endif // __SEC_STD_PATHS_H
 
