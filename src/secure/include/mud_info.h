00129731ae05e3881c626f8ac3c9eaee373b0df4
Shea690901 <ginny690901@hotmail.de>
2016-02-04 21:52:24 +0100

    <ref>(mud_info.h): update

--- src/secure/include/mud_info.h
+++ src/secure/include/mud_info.h
@@ -12,11 +12,13 @@
 #define MUD_INFO_SAVE     __SAVE_FILE(PRIV_SAVE_DIR "mud_info")
 
 // mapping player_info {{{
-#define MI_PI_CRED      0       ///< credentials (uid:gid) of the player (string)
-#define MI_PI_INVALID   1       ///< supplied name invalid (string)
+#define MI_PI_CRED      0       ///< credentials (uid:gid) of the player    (string)
+#define MI_PI_INVALID   1       ///< supplied name invalid                  (string)
 #define MI_PI_PWDC      2       ///< counter how many tries for passwd left (int)
-#define MI_PI_PWD       3       ///< crypted passwd (string)
-#define MI_PI_REG       4       ///< registered email supplied (int)
+#define MI_PI_PWD       3       ///< crypted passwd                         (string)
+#define MI_PI_REG       4       ///< registered email supplied              (int)
+#define MI_PI_NAME      5       ///< name for new players                   (string)
+#define MI_PI_GENDER    6       ///< gender for new players                 (string)
 // }}}
 // email handling {{{
 #define MI_EMAIL_INVALID    "@inv@"
